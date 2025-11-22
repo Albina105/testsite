8fb3f2e155ebda05928ee201a971307f46d623cc
Albina105 <ajtmurzaevaalbjna@gmail.com>
2025-11-22 22:27:19 +0600

    I have finished my project

---
+++ myproject/lalafo_app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30 , unique=True)
+    category_image = models.ImageField(upload_to='cat_images/')
+
+    def __str__(self):
+        return f'Категоринин аты: {self.category_name}'
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100 , unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    owner = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='product_photo',null=True,blank=True)
+    phone_number = PhoneNumberField(null=True,blank=True)
+    product_type = models.BooleanField()
+    description = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
---
+++ myproject/lalafo_app/pagination.py
@@ -0,0 +1,8 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class CategoryPagination(PageNumberPagination):
+    page_size = 4
+
+class ProductPagination(PageNumberPagination):
+    page_size = 3
